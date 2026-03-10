32fb1d7b371155d29ad33bfb208620fec92cf7d7
F.Castana <f.castana@araneum.it>
2026-03-10 11:40:13 +0100

    first step new demo

---
+++ SYS-05-web-component-programming/web_component/demo/config/initializers/sidekiq.rb
@@ -0,0 +1,12 @@
+conf = { url: ENV['REDIS_URL'] }
+conf.merge!(password: ENV['REDIS_PASSWORD']) unless ENV['REDIS_PASSWORD'].blank?
+
+Sidekiq.configure_server do |config|
+  config.redis = conf
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = conf
+end
+
+Sidekiq.strict_args!
---
+++ SYS-05-web-component-programming/web_component/demo/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # Health check endpoint
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Main routes - Demo selection
+  root "home#index"
+
+  # Demo 1
+  get "demo1", to: "demo1#index", as: :demo1
+  post "demo1/configure", to: "demo1#configure", as: :demo1_configure
+end
